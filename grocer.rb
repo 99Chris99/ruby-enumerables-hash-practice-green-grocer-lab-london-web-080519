ae02c75e0cb934925ffdc6f3dea87c30b07256c4
Chris Hodge <chrishodge87@gmail.com>
2019-07-19 14:47:33 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -86,13 +86,15 @@ def checkout(cart, coupons)
 part_a_cart = consolidate_cart(cart)
 part_b_cart = apply_coupons(part_a_cart, coupons)
 final_cart = apply_clearance(part_b_cart)
-total_price = 5.00
+total_price = 0.00
 
-#final_cart.each do|item, hash|
+final_cart.each do|item, hash|
 
-#total_price = hash[:price]
+total_price += hash[:price]
 
-#end
+
+
+end
 
 return total_price
 
